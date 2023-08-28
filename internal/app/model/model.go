1b80f1d7b878cb6bf8e347e040656c4c6d3bc96c
psxzz <m.martusevich@yandex.ru>
2023-08-28 17:26:03 +0300

    Add models

---
+++ internal/app/model/model.go
@@ -0,0 +1,21 @@
+package model
+
+// type User struct {
+// 	ID int64
+// }
+
+type Segment struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type UserExperiment struct {
+	ID        int64 `json:"id"`
+	UserID    int64 `json:"user_id"`
+	SegmentID int64 `json:"segment_id"`
+}
+
+type UserExperimentList struct {
+	ID       int64     `json:"user_id"`
+	Segments []Segment `json:"segments"`
+}
