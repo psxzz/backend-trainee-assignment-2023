cbfc3428be13647b9cf2afbdf4a2a6d943745c8c
psxzz <m.martusevich@yandex.ru>
2023-08-29 15:27:09 +0300

    Add endpoint layer

--- internal/app/endpoint/endpoint.go
+++ internal/app/endpoint/endpoint.go
@@ -1,10 +1,21 @@
 package endpoint
 
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/psxzz/backend-trainee-assignment/internal/app/model"
+	"github.com/psxzz/backend-trainee-assignment/internal/app/storage"
+)
+
 type Service interface {
-	CreateSegment()
-	DeleteSegment()
-	AddUser()
-	ListUser()
+	CreateSegment(context.Context, string) (*model.Segment, error)
+	DeleteSegment(context.Context, string) (*model.Segment, error)
+	AddUserExperiments(context.Context, int64, []string) ([]*model.UserExperiment, error)
+	RemoveUserExperiments(context.Context, int64, []string) ([]*model.UserExperiment, error)
+	ListUserSegments(context.Context, int64) (*model.UserExperimentList, error)
 }
 
 type Endpoint struct {
@@ -17,18 +28,137 @@ func New(svc Service) *Endpoint {
 	}
 }
 
-// func (e *Endpoint) CreateSegment(ctx echo.Context) error {
+func (e *Endpoint) HandleCreate(ctx echo.Context) error {
+	var req segmentRequest
+	if err := ctx.Bind(&req); err != nil {
+		return err
+	}
 
-// }
+	if err := ctx.Validate(req); err != nil {
+		return ctx.JSON(http.StatusBadRequest, errorResponse{
+			Message: "field 'name' not found",
+		})
+	}
 
-// func (e *Endpoint) DeleteSegment(ctx echo.Context) error {
+	segment, err := e.svc.CreateSegment(ctx.Request().Context(), req.Name)
+	if err != nil {
+		if errors.Is(err, storage.ErrSegmentExists) {
+			return ctx.JSON(http.StatusBadRequest, errorResponse{
+				Message: errors.Unwrap(err).Error(),
+			})
+		}
 
-// }
+		return ctx.JSON(http.StatusInternalServerError, errorResponse{
+			Message: "Internal error",
+		})
+	}
 
-// func (e *Endpoint) AddSegment(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, segment)
+}
 
-// }
+func (e *Endpoint) HandleDelete(ctx echo.Context) error {
+	var req segmentRequest
+	if err := ctx.Bind(&req); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, errorResponse{
+			Message: "Internal error",
+		})
+	}
 
-// func (e *Endpoint) UserSegments(ctx echo.Context) error {
+	if err := ctx.Validate(req); err != nil {
+		return ctx.JSON(http.StatusBadRequest, errorResponse{
+			Message: "field 'name' not found",
+		})
+	}
 
-// }
+	segment, err := e.svc.DeleteSegment(ctx.Request().Context(), req.Name)
+	if err != nil {
+		if errors.Is(err, storage.ErrSegmentNotFound) {
+			return ctx.JSON(http.StatusBadRequest, errorResponse{
+				Message: errors.Unwrap(err).Error(),
+			})
+		}
+
+		return ctx.JSON(http.StatusInternalServerError, errorResponse{
+			Message: "Internal error",
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, segment)
+}
+
+func (e *Endpoint) HandleExperiments(ctx echo.Context) error {
+	var req userExperimentRequest
+	if err := ctx.Bind(&req); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, errorResponse{
+			Message: "Internal error",
+		})
+	}
+
+	if err := ctx.Validate(req); err != nil {
+		return ctx.JSON(http.StatusBadRequest, errorResponse{
+			Message: "Invalid request body",
+		})
+	}
+
+	added, err := e.svc.AddUserExperiments(ctx.Request().Context(), req.UserID, req.ToAdd)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, errorResponse{
+			Message: "Internal error",
+		})
+	}
+
+	removed, err := e.svc.RemoveUserExperiments(ctx.Request().Context(), req.UserID, req.ToRemove)
+	if err != nil {
+		return ctx.JSON(http.StatusInternalServerError, errorResponse{
+			Message: "Internal error",
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, userExperimentResponse{
+		UserID:  req.UserID,
+		Added:   added,
+		Removed: removed,
+	})
+}
+
+func (e *Endpoint) HandleUserExperimentList(ctx echo.Context) error {
+	var req experimentListRequest
+	if err := ctx.Bind(&req); err != nil {
+		return ctx.String(http.StatusInternalServerError, "Internal error")
+	}
+
+	if err := ctx.Validate(req); err != nil {
+		return ctx.String(http.StatusBadRequest, "Invalid request body")
+	}
+
+	list, err := e.svc.ListUserSegments(ctx.Request().Context(), req.UserID)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, list)
+}
+
+type errorResponse struct {
+	Message string `json:"message"`
+}
+
+type segmentRequest struct {
+	Name string `json:"name" validate:"required"`
+}
+
+type userExperimentRequest struct {
+	UserID   int64    `json:"user_id" validate:"required"`
+	ToAdd    []string `json:"to_add" validate:"required"`
+	ToRemove []string `json:"to_remove" validate:"required"`
+}
+
+type userExperimentResponse struct {
+	UserID  int64                   `json:"user_id"`
+	Added   []*model.UserExperiment `json:"added"`
+	Removed []*model.UserExperiment `json:"removed"`
+}
+
+type experimentListRequest struct {
+	UserID int64 `json:"user_id" validate:"required"`
+}
