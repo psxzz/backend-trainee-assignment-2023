6f25e944803da028c64ac487b21933bac65da9ee
psxzz <m.martusevich@yandex.ru>
2023-08-30 19:38:15 +0300

    Update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import "github.com/ilyakaznacheev/cleanenv"
 
 type Config struct {
+	LogsPath    string `env:"AVITO_LOG_PATH" env-default:"./logs/"`
 	DatabaseDSN string `env:"AVITO_DATABASE_DSN" env-default:"postgres://postgres:postgres@localhost:5432/experimental_segments?sslmode=disable"`
 }
 
