5d3f1d769ca713cd99ccac05f4a44ad660d3e58f
psxzz <m.martusevich@yandex.ru>
2023-08-30 19:42:16 +0300

    Update migrations

--- migrations/user_experiments.sql
+++ migrations/user_experiments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_experiments (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    user_id INTEGER,
-    segment_id INTEGER REFERENCES Segments(id),
+    user_id INTEGER NOT NULL,
+    segment_id INTEGER NOT NULL REFERENCES Segments(id),
     UNIQUE(user_id, segment_id)
 );
